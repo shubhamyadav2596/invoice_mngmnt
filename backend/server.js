4710114524184ba7336d8ad27193b0e9c302feac
progammerBoy <sy8692935@gmail.com>
2025-03-09 21:40:45 +0530

    fixed backend issue

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,16 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cache-Control",
+    "Expires",
+    "Pragma",
+  ],
+  credentials: true,
 }));
 app.use(express.json());
 
